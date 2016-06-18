d4121e4acf7ba9f46fc5213493f140de63c3204a
Andrew Taylor <mac@andrewt.net>
2016-06-18 15:27:45 +0100

    About to add real colour transforms

--- main.js
+++ main.js
@@ -7,9 +7,24 @@ var pixWidth = 10,
 	yOffset = 1,
 	channelOffset = 4,
 	subPixSize = 1,
+	gamma = 1.2,
 	pixGap = 0,
 	subPixGap = 0,
-	cols = [ '#f00', '#0f0', '#00f' ];
+	cols = [ '#a91b0f', '#012c19', '#0a2471' ],
+	multipliers = [ 0xff / 0xa9, 0xff / 0x2c, 0xff / 0x71 ],
+	black = '#121011',
+	frame = '#000000';
+
+let maxMultiplier = Math.max(...multipliers);
+multipliers = multipliers.map(x => x / maxMultiplier);
+
+let sRGB2penRGB = cols.map(col => {
+		let row = [];
+		for (let i = 0; i < 3; ++i)
+			row.push(parseInt(col.substr(i * 2 + 1, 2), 16) / 0xff);
+		return row;
+	}),
+	penRGB2sRGB = matrix_invert(sRGB2penRGB);
 
 document.addEventListener('DOMContentLoaded', e => {
 	let canvas = document.getElementById('canvas'),
@@ -23,7 +38,7 @@ document.addEventListener('DOMContentLoaded', e => {
 		canvas.width = pixSize * image.width;
 		canvas.height = pixSize * image.height;
 		ctx = canvas.getContext('2d');
-		box(0, 0, canvas.width, canvas.height, '#444');
+		// box(0, 0, canvas.width, canvas.height, frame);
 		for (let x = 0; x < image.width; ++x)
 			for (let y = 0; y < image.height; ++y) {
 				let i = (x + y * image.height) * 4,
@@ -31,9 +46,13 @@ document.addEventListener('DOMContentLoaded', e => {
 						imageData.data[i],
 						imageData.data[i + 1],
 						imageData.data[i + 2]
-					].map(c => (c / 255) * pixWidth * pixHeight),
+					].map((c, i) => {
+						let max = pixWidth * pixHeight,
+							raw = Math.pow(c / 255, gamma) * max * multipliers[i];
+						return raw > max ? max : raw;
+					}),
 					grid = pixelizer(pixel);
-				box(x * pixSize, y * pixSize, pixSize - pixGap, pixSize - pixGap, 'black');
+				box(x * pixSize, y * pixSize, pixSize - pixGap, pixSize - pixGap, frame);
 				cols.forEach((col, i) => {
 					for (let py = 0; py < pixHeight; ++py)
 						for (let px = 0; px < pixWidth; ++px)
@@ -41,7 +60,7 @@ document.addEventListener('DOMContentLoaded', e => {
 								y * pixSize + yOffset + py * subPixSize + channelOffset * i,
 								subPixSize - subPixGap,
 								subPixSize - subPixGap,
-								grid[i][py][px] ? col : '#444');
+								grid[i][py][px] ? col : black);
 				});
 			}
 
